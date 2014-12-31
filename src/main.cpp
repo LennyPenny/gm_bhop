2503d689a6c14a51c02e3bd3c79e3af39ae6b25d
LennyPenny <l.bernhardt@live.de>
2014-12-31 03:43:05 +0100

    cleaned up code a bit

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 
 #include "cbase.h"
 
-
 #include "getInterface.h"
 #include "SigScan.h"
 #include "Hook/VMT.h"
@@ -24,55 +23,54 @@ IBaseClientDLL *baseClient;
 IVEngineClient *client;
 IClientEntityList *list;
 
-CNetworkedVariableManager *netVarMngr;
+CNetworkedVariableManager netVarMngr;
 
-SigScan clientScan = SigScan("client.dll");
+const int CREATEMOVE_OFFSET = 21;
 
-int CREATEMOVE_OFFSET = 21;
+Hook::VMT *clientModeSharedHooks;
 
-Hook::VMT *ClientModeSharedHooks;
+int flagsOffset;
 
-int m_fFlagsOffset;
-
-typedef bool ( __thiscall *hCreateMoveFn )(void *, float, CUserCmd *);
-bool __fastcall hCreateMove(void *_this, void *, float inputSampleTime, CUserCmd *cmd) {
+typedef bool ( __thiscall *hookedCreateMoveFn )(void *, float, CUserCmd *);
+bool __fastcall hookedCreateMove(void *_this, void *, float inputSampleTime, CUserCmd *cmd) {
 
 	int *lply = (int *) list->GetClientEntity(client->GetLocalPlayer())->GetBaseEntity(); 
 
-	if (!m_fFlagsOffset)
-		m_fFlagsOffset = netVarMngr->GetOffset("DT_BasePlayer", "m_fFlags");
+	if (!flagsOffset)
+		flagsOffset = netVarMngr.GetOffset("DT_BasePlayer", "m_fFlags");
 
-	int flags = *(int *)((char *)(lply) + m_fFlagsOffset);
+	int flags = *(int *)((char *)(lply) + flagsOffset);
 
 	if ( cmd->buttons & IN_JUMP && !(flags & FL_ONGROUND))
 			cmd->buttons &= ~IN_JUMP;
 
-	return ClientModeSharedHooks->GetOriginal<hCreateMoveFn>(CREATEMOVE_OFFSET)( _this, inputSampleTime, cmd );
+	return clientModeSharedHooks->GetOriginal<hookedCreateMoveFn>(CREATEMOVE_OFFSET)( _this, inputSampleTime, cmd );
 }
 GMOD_MODULE_OPEN() {
 	baseClient = GetInterface<IBaseClientDLL *>("client.dll", CLIENT_DLL_INTERFACE_VERSION);
-	if ( !baseClient )
-		Warning("Couldn't get client.dll interface");
+	if (!baseClient)
+		return 0;
 
 	client = GetInterface<IVEngineClient *>("engine.dll", VENGINE_CLIENT_INTERFACE_VERSION);
-	if ( !client )
-		Warning("Couldn't get EngineClient interface");
+	if (!client)
+		return 0;
 
 	list = GetInterface<IClientEntityList *>("client.dll", VCLIENTENTITYLIST_INTERFACE_VERSION);
-	if ( !list )
-		Warning("Couldn't get EntityList interface");
+	if (!list)
+		return 0;
 
 
-	void *g_ClientModeShared = **( void *** ) clientScan.Scan("\x8B\x0D????\x50\x8B\x01\x8B\x50\x24", 2);
+	void *clientModeShared = **( void *** ) SigScan("client.dll").Scan("\x8B\x0D????\x50\x8B\x01\x8B\x50\x24", 2);
 
-	netVarMngr = new CNetworkedVariableManager(baseClient->GetAllClasses());
+	netVarMngr = CNetworkedVariableManager(baseClient->GetAllClasses());
 
-	ClientModeSharedHooks = new Hook::VMT(g_ClientModeShared);
-	ClientModeSharedHooks->Hook(CREATEMOVE_OFFSET, hCreateMove);
+	clientModeSharedHooks = new Hook::VMT(clientModeShared);
+	clientModeSharedHooks->Hook(CREATEMOVE_OFFSET, hookedCreateMove);
 
 	return 0;
 }
 
 GMOD_MODULE_CLOSE() {
+	clientModeSharedHooks->Remove();
 	return 0;
 }
